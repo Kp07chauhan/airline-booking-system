879cce5351620a713d2b6a18e32460cae47ce72e
Kapil Kumar Chauhan <kp0007chauhan@gmail.com>
2026-04-23 15:31:59 +0530

    Add import for Value annotation in JwtUtil

--- src/main/java/com/airline/booking/security/JwtUtil.java
+++ src/main/java/com/airline/booking/security/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
 import java.util.Date;
